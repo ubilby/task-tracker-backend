34aa544a9e289a346d9174cd2bbd083b6bd32db8
ubilby <ubilby@gmail.com>
2025-11-06 01:47:15 +0300

    removed exceptions from main for cleaner code

---
+++ application/exception_handlers.py
@@ -0,0 +1,34 @@
+from fastapi.responses import JSONResponse
+
+from exceptions import UserNotFoundError, TaskNotFoundError
+from fastapi.responses import JSONResponse
+
+
+async def value_error_exception_handler(request, exc: ValueError):
+    """Преобразует Python's ValueError в HTTP 400 Bad Request."""
+    return JSONResponse(
+        status_code=400,
+        content={"detail": str(exc)},
+    )
+
+
+async def user_not_found_exception_handler(request, exc: UserNotFoundError):
+    """Преобразует UserNotFoundError в HTTP 404 Not Found."""
+    return JSONResponse(
+        status_code=404,
+        content={"detail": str(exc)},
+    )
+
+async def task_not_found_exception_handler(request, exc: TaskNotFoundError):
+    """Преобразует TaskNotFoundError в HTTP 404 Not Found."""
+    return JSONResponse(
+        status_code=404,
+        content={"detail": str(exc)},
+    )
+
+
+CUSTOM_EXCEPTION_HANDLERS = {
+    ValueError: value_error_exception_handler,
+    UserNotFoundError: user_not_found_exception_handler,
+    TaskNotFoundError: task_not_found_exception_handler,
+}
--- main.py
+++ main.py
@@ -1,41 +1,14 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 
+from application.exception_handlers import CUSTOM_EXCEPTION_HANDLERS
 from user.api.router import user_router
 from task.api.router import task_router
-from exceptions import UserNotFoundError, TaskNotFoundError
 
 
-app = FastAPI(title="TaskTracker API")
+app = FastAPI(
+    title="TaskTracker API",
+    exception_handlers=CUSTOM_EXCEPTION_HANDLERS    
+)
 app.include_router(user_router)
 app.include_router(task_router)
 
-
-@app.exception_handler(ValueError)
-async def value_error_exception_handler(request, exc: ValueError):
-    """
-    Этот обработчик будет ловить ВСЕ ошибки ValueError,
-    вылетающие из твоего приложения, и превращать их в 400 Bad Request.
-    """
-    return JSONResponse(
-        status_code=400,
-        content={"detail": str(exc)},
-    )
-
-
-@app.exception_handler(UserNotFoundError)
-async def user_not_found_exception_handler(request, exc: ValueError):
-
-    return JSONResponse(
-        status_code=404,
-        content={"detail": str(exc)},
-    )
-
-
-@app.exception_handler(TaskNotFoundError)
-async def task_not_found_exception_handler(request, exc: ValueError):
-
-    return JSONResponse(
-        status_code=404,
-        content={"detail": str(exc)},
-    )
